02168fd54c3a943afeac769fb323568d8092f6c9
qtran46 <quangdoannhat.tran@dxc.com>
2023-07-09 22:47:11 +0700

    ci

--- src/Utils/formatLocaleNumber.js
+++ src/Utils/formatLocaleNumber.js
@@ -1,5 +1,3 @@
-const VNFormat = 'vi-VN';
-const ENFormat = 'en-IN';
 export const FormatLocaleNumber = (number, numberToFixed = 0) => {
   console.log(number)
   return +number.toLocaleString(undefined)
